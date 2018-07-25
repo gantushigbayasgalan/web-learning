eb48014867aece860010da742bb9c86e30d810bd
gantushigbayasgalan <gantushig19941221@gmail.com>
2018-07-25 15:07:02 +0800

    Second commit

--- public/main.js
+++ public/main.js
@@ -1,6 +1,15 @@
-var count = 0;
+const rootRef = firebase.database().ref();
+const pageCountsRef = rootRef.child("pageCounts");
 
-function myFunction() {
-    count++;
-    document.getElementById('demo').innerHTML = "hello " +  count * 100;
-}
+var postData = {
+    page: location.pathname,
+    count: 1, 
+    lastvisit: firebase.database.ServerValue.TIMESTAMP,
+    lastreferrer: document.referrer
+}; 
+
+pageCountsRef.push(postData);
+
+// Create an object to store copy of the saved db data
+let obj = {};
+
